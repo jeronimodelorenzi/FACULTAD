e78980ebe7b5c2151b5de4f4ee8339a09498aa47
jeronimodelorenzi <jerobeatle@gmail.com>
2025-05-17 10:08:08 -0300

    SO P3

---
+++ Tercer Año/Sistemas Operativos I/Practica 3/Ejercicio 4/ej4a.c
@@ -0,0 +1,79 @@
+/*
+El problema de los lectores y escritores consiste en M hilos
+lectores y N escritores tratando de acceder a un arreglo en memoria compartida con las siguientes
+restricciones:
+No puede haber un lector accediendo al arreglo al mismo tiempo que un escritor.
+Varios lectores pueden acceder al arreglo simult´aneamente.
+S´olo puede haber un escritor a la vez.
+
+La soluci´on a este problema es esencialmente un read-write lock. Los siguientes apartados piden implementar
+distintas variantes. Las mismas deben ser reusables: el read-write lock no debe estar atado de ninguna
+forma a este problema en particular (e.g. no debe saber las cantidades de escritores y lectores, ni asumir
+que un proceso es siempre lector o siempre escritor). Organice su respuesta separando claramente la
+implementaci´on del lock de su uso en el problema (idealmente en archivos separados). La interfaz deber´ıa
+ser similar a la ofrecida por los mutex de pthread.
+*/
+
+/*
+a) Implemente una soluci´on y expl´ıquela. Para este apartado, siempre que el lock est´e tomado por un
+lector y aparezca un segundo lector, el segundo debe poder entrar inmediatamente (read-preferring). 
+*/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+#define M 5
+#define N 5
+#define ARRLEN 10240
+
+int arr[ARRLEN];
+
+void *escritor(void *arg) {
+  int i;
+  int num = arg - (void *)0;
+
+  while (1) {
+    sleep(random() % 3);
+
+    printf("Escritor %d escribiendo\n", num);
+    for (i = 0; i < ARRLEN; i++)
+      arr[i] = num;
+  }
+  return NULL;
+}
+
+void *lector(void *arg) {
+  int v, i;
+  int num = arg - (void *)0;
+
+  while (1) {
+    sleep(random() % 3);
+
+    v = arr[0];
+    for (i = 1; i < ARRLEN; i++) {
+      if (arr[i] != v)
+        break;
+    }
+    if (i < ARRLEN)
+      printf("Lector %d, error de lectura\n", num);
+    else
+      printf("Lector %d, dato %d\n", num, v);
+  }
+  return NULL;
+}
+
+int main() {
+  pthread_t lectores[M], escritores[N];
+  int i;
+
+  for (i = 0; i < M; i++)
+    pthread_create(&lectores[i], NULL, lector, i + (void *)0);
+  for (i = 0; i < N; i++)
+    pthread_create(&escritores[i], NULL, escritor, i + (void *)0);
+
+  pthread_join(lectores[0], NULL); /* Espera para siempre */
+  
+  return 0;
+}
---
+++ Tercer Año/Sistemas Operativos I/Practica 3/Ejercicio 4/rwlock_vc.h
@@ -0,0 +1,44 @@
+#ifndef __RWLOCK_VC_H__
+#define __RWLOCK_VC_H__
+
+#include <pthread.h>
+
+typedef struct _rwlock_t {
+  pthread_mutex_t mutex;
+  pthread_cond_t lectores;
+  pthread_cond_t escritores;
+  int cant_lectores, cant_escritores;
+} rwlock_t;
+
+/**
+ * Inicializa los valores de la estructura rwlock_t.
+ */
+void rwlock_init (rwlock_t *rw);
+
+/**
+ * Lock a lectores.
+ */
+void rwlock_rlock (rwlock_t *rw);
+
+/**
+ * Unlock a lectores.
+ */
+void rwlock_runlock (rwlock_t *rw);
+
+/**
+ * Lock a escritores. 
+ */
+void rwlock_wlock (rwlock_t *rw);
+
+/**
+ * Unlock a escritores.
+ */
+void rwlock_wunlock (rwlock_t *rw);
+
+/**
+ * Destruye la estructura rwlock_t.
+ */
+
+void rwlock_destroy (rwlock_t *rw);
+
+#endif
