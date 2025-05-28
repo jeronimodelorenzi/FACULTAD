1526f6c3ede96cfca8caa9856ecf7ba313203aa1
jeronimodelorenzi <jerobeatle@gmail.com>
2025-05-28 19:30:33 -0300

    SO P4

--- Tercer Año/Sistemas Operativos I/Practica 4/ej5.c
+++ Tercer Año/Sistemas Operativos I/Practica 4/ej5.c
@@ -2,24 +2,42 @@
 #include <stdlib.h>
 #include <omp.h>
 #include "timing.h"
-#define N 1000
+#define N 4
 
 int A[N][N], B[N][N], C[N][N];
+int BT[N][N];
 
 void mult(int A[N][N], int B[N][N], int C[N][N]) {
   
   int i, j, k;
-  #pragma omp parallel 
-  {
-    #pragma omp for private(i,j,k)
-    for (i = 0; i < N; i++)
-      for (j = 0; j < N; j++)
-        for (k = 0; k < N; k++)
+  #pragma omp parallel for private(i,j,k)
+    for (i = 0; i < N; i++) {
+      for (j = 0; j < N; j++) {
+        C[i][j] = 0;
+        for (k = 0; k < N; k++) {
           C[i][j] += A[i][k] * B[k][j];
-  }
+        }
+      }
+    }
 }
 
-void multsec(int A[N][N], int B[N][N], int C[N][N]) {
+/*Más rápido porque se accede por filas ambas matrices. */
+void mult_traspuesta(int A[N][N], int B[N][N], int C[N][N]) {
+  int i, j, k;
+
+  #pragma omp parallel for private(i,j,k)
+    for (i = 0; i < N; i++) {
+      for (j = 0; j < N; j++) {
+        int sum = 0;
+        for (k = 0; k < N; k++) {
+          sum += A[i][k] * B[j][k];
+        }
+        C[i][j] = sum;
+      }
+    }
+}
+
+void mult_sec(int A[N][N], int B[N][N], int C[N][N]) {
   int i, j, k;
   
   for (i = 0; i < N; i++)
@@ -32,22 +50,20 @@ int main() {
   int i, j;
   for (i = 0; i < N; i++) {
     for (j = 0; j < N; j++) {
-      A[i][j] = random() % 2;
-      B[i][j] = random() % 2;
+      A[i][j] = i;
+      B[i][j] = j;
     }
   }
   omp_set_num_threads(4);
 
-  mult(A, B, C);
-
   float fv;
-  TIME_void(mult(A,B,C), &fv);
+  TIME_void(mult_traspuesta(A,B,C), &fv);
 
-//   for (i = 0 ; i < N ; i++) {
-//     for (j = 0 ; j < N ; j++) {
-//       printf("C[%d][%d] = %d\n", i, j, C[i][j]);
-//     }
-//   }
+  for (i = 0 ; i < N ; i++) {
+    for (j = 0 ; j < N ; j++) {
+      printf("C[%d][%d] = %d\n", i, j, C[i][j]);
+    }
+  }
 
   return 0;
 }
