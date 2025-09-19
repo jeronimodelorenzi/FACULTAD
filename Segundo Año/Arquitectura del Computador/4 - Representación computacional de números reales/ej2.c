d57080e86da93a8b797250d5e12336e0f9a372e2
jeronimodelorenzi <jerobeatle@gmail.com>
2025-09-19 09:12:30 -0300

    Arquitectura P1

--- Segundo Año/Arquitectura del Computador/4 - Representación computacional de números reales/ej2.c
+++ Segundo Año/Arquitectura del Computador/4 - Representación computacional de números reales/ej2.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <ieee754.h>
 
-#define EXPONENTE(x) (((*(int*)&(f)) >> 23) & 0xFF)
-#define FRACCION(x) (((*(int*)&(f)) & 0x7FFFFF))
+#define EXPONENTE(x) (((*(int*)&(x)) >> 23) & 0xFF)
+#define FRACCION(x) (((*(int*)&(x)) & 0x7FFFFF))
 
 int main () {
-  float f = -138;
+  float f = -138.0f;
 
   float exp = EXPONENTE(f);
   float frac = FRACCION(f);
@@ -12,6 +13,12 @@ int main () {
   printf("Numero: %f\n", f);
   printf("Exponente: %f\n", exp);
   printf("Fracción: %f\n", frac);
+
+  union ieee754_float u;
+  u.f = -138.0f;
+  printf("Número ieee: %f\n", u.f);
+  printf("Exponente ieee: %d\n", u.ieee.exponent);
+  printf("Fracción ieee: %d\n", u.ieee.mantissa);
 
   return 0;
 }
