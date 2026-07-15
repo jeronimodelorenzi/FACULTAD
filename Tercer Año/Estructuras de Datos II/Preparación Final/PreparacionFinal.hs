d4e900c2ff64f1677335be9c47a24ac216e36943
jeronimodelorenzi <jerobeatle@gmail.com>
2026-07-15 12:11:37 -0300

    Preparación final edya

--- Tercer Año/Estructuras de Datos II/Preparación Final/PreparacionFinal.hs
+++ Tercer Año/Estructuras de Datos II/Preparación Final/PreparacionFinal.hs
@@ -306,3 +306,401 @@ ls p t = ls' ("/":p) t
 
 dirtree = Dir "/" [Dir "home" [Dir "luis" [File "Carta.txt" "xxx"], Dir "Pedro" []], Dir "mnt" []]
 
+{-
+Final 1 - 2019
+-}
+data BTree a = E6 | L6 a | N6 Int (BTree a) (BTree a)
+
+stripSufix :: Eq a => BTree a -> BTree a -> Maybe (BTree a)
+stripSufix suf sec | size2 suf > size2 sec = Nothing
+                   | otherwise = let
+                                    k = size2 sec - size2 suf
+                                    (prefijo, sufijo) = divide sec k
+                                 in
+                                    if iguales suf sufijo then Just sufijo else Nothing
+    where
+        size2 :: BTree a -> Int
+        size2 E6 = 0
+        size2 (L6 x) = 1
+        size2 (N6 s _ _) = s
+
+        divide :: BTree a -> Int -> (BTree a, BTree a)
+        divide E6 _ = (E6, E6)
+        divide (L6 x) i      | i < 1 = (E6, L6 x)
+                             | otherwise = (L6 x, E6)
+        divide t@(N6 s l r) i | i < 1   = (E6, t)
+                              | i >= s  = (t, E6)
+                              | i <= size2 l = let (l', r') = divide l i
+                                         in (l', append2 r' r)
+                              | otherwise = let (l', r') = divide r i
+                                           in (append2 l l', r')
+
+        append2 :: BTree a -> BTree a -> BTree a
+        append2 E6 t2 = t2
+        append2 t1 E6 = t1
+        append2 t1 t2 = N6 (size2 t1 + size2 t2) t1 t2
+
+        iguales :: Eq a => BTree a -> BTree a -> Bool
+        iguales E6 E6 = True
+        iguales (L6 x) (L6 y) = x == y
+        iguales (N6 _ l1 r1) (N6 _ l2 r2) = let
+                                                (bL, bR) = iguales l1 l2 ||| iguales r1 r2
+                                            in
+                                                bL && bR
+
+maxBalance :: Seq s => s Int -> Int
+maxBalance s =  let
+                    (s_red, red) = scanS (+) 0 s
+                    s_append = appendS s_red (singletonS red)
+                    (s_min, _) = scanS min maxBound s_append
+                    s_res = tabulateS (\i -> nthS s_append i - nthS s_min i) (lengthS s_append) `asTypeOf` s_append
+                in
+                    reduceS max minBound s_res
+
+{-
+Ejercicios finales diciembre x
+-}
+data Tree2 a = E | L a | N Int (Tree2 a) (Tree2 a) deriving Show
+
+size3 :: Tree2 a -> Int
+size3 E = 0
+size3 (L _) = 1
+size3 (N s _ _) = s
+
+append3 :: Tree2 a -> Tree2 a -> Tree2 a
+append3 E t2 = t2
+append3 t1 E = t1
+append3 t1 t2 = N (size3 t1 + size3 t2) t1 t2
+
+filterInd :: (a -> Bool) -> Tree2 a -> Tree2 (Int, a)
+filterInd f t = filterIndAux f t 0
+    where
+        filterIndAux :: (a -> Bool) -> Tree2 a -> Int -> Tree2 (Int, a)
+        filterIndAux _ E _ = E
+        filterIndAux f (L x) i | f x = L (i, x)
+                               | otherwise = E
+        filterIndAux f (N _ l r) i = let
+                                        sL = size3 l
+                                        (l', r') = filterIndAux f l i ||| filterIndAux f r (i+sL)
+                                     in
+                                        append3 l' r'
+
+partir :: (Num a, Ord a) => Tree2 a -> (Tree2 (Int,a), Tree2 (Int, a))
+partir t = partirAux t 0
+    where
+        partirAux :: (Num a, Ord a) => Tree2 a -> Int -> (Tree2 (Int,a), Tree2 (Int, a))
+        partirAux E _ = (E, E)
+        partirAux (L x) i | x >= 0 = (L (i,x), E)
+                          | otherwise = (E, L (i, x))
+        partirAux (N _ l r) i = let
+                                    sL = size3 l
+                                    ((lP, lN), (rP, rN)) = partirAux l i ||| partirAux r (i+sL)
+                                in
+                                    (append3 lP rP, append3 lN rN) 
+                                    
+
+cantCeros :: Seq s => s Int -> Int
+cantCeros s = let
+                s_info = tabulateS (\i -> if nthS s i == 0 then 1 else 0) (lengthS s) `asTypeOf` s
+              in
+                reduceS (+) 0 s_info
+
+posUnos :: Seq s => s Int -> s Int
+posUnos s = let
+                ceros = cantCeros s
+                (s_red, _) = scanS (+) 0 s
+            in
+                tabulateS (\i -> ceros + nthS s_red i + nthS s i - 1) (lengthS s) `asTypeOf` s
+
+posCeros :: Seq s => s Int -> s Int
+posCeros s = let
+                ceros = tabulateS (\i -> if nthS s i == 0 then 1 else 0) (lengthS s) `asTypeOf` s
+                (s_red, _) = scanS (+) 0 ceros
+             in
+                tabulateS (\i -> if nthS s i == 0 then nthS s_red i else max 0 (nthS s_red i-1)) (lengthS s)
+
+posiciones :: Seq s => s Int -> s Int
+posiciones s =  let
+                    posicionesCero = posCeros s
+                    posicionesUno = posUnos s
+                in
+                    tabulateS (\i-> if nthS s i == 0 then nthS posicionesCero i else nthS posicionesUno i) (lengthS s) `asTypeOf` s
+
+type Doc s = (Int, s String)
+type Index s = s (String, s (Int, Int))
+
+makeIndex :: Seq s => s (Doc s) -> Index s
+makeIndex s = let
+                s_pares = mapS (\(docId, palabras) -> 
+                                  tabulateS (\i -> (nthS palabras i, (docId, i))) (lengthS palabras)
+                               ) s
+                s_flatten = reduceS appendS emptyS s_pares
+              in
+                collectS s_flatten
+
+moreUsed :: Seq s => Index s -> String
+moreUsed s = let
+                s_info = mapS (\(string, datos) -> (string, lengthS datos)) s
+                compare t1@(str1, cant1) t2@(str2, cant2) = if cant1 >= cant2 then t1 else t2
+                (strFinal, _) = reduceS compare ("",0) s_info
+             in
+                strFinal
+
+incluidos :: Seq s => s (Int, Int) -> Int
+incluidos s = let
+                s_in = mapS (\(x,y) -> x + y) s
+                (maxs, _) = scanS max minBound s_in
+                cont = tabulateS (\i -> 
+                    if i == 0 
+                    then 0 
+                    else if nthS s_in i <= nthS maxs i 
+                         then 1 
+                         else 0
+                    ) (lengthS s) `asTypeOf` s_in
+              in
+                reduceS (+) 0 cont
+
+{-
+Ejercicios finales diciembre x
+-}
+entradas :: Seq s => s Int -> Int -> (Float, Int)
+entradas s x = let
+                (s_sum, _) = scanS (+) 0 s
+                s_info = tabulateS (\i -> if i == 0 then (fromIntegral (nthS s i * x), 0)
+                                          else if fromIntegral (nthS s_sum i) / fromIntegral i >= fromIntegral (nthS s (i-1)) 
+                                            then (fromIntegral (nthS s i) * 0.5 * fromIntegral x, nthS s i)
+                                            else (fromIntegral (nthS s i) * fromIntegral x, 0)) (lengthS s) `asTypeOf` mapS undefined s
+               in
+                reduceS (\(s1,d1) (s2, d2) -> (s1+s2, d1+d2)) (0,0) s_info
+
+split :: (String -> Bool) -> Tree2 String -> (Tree2 (Int, String), Tree2 (Int, String))
+split f t = splitAux f t 0
+    where
+        splitAux :: (String -> Bool) -> Tree2 String -> Int -> (Tree2 (Int, String), Tree2 (Int, String))
+        splitAux _ E _ = (E, E)
+        splitAux f (L x) i | f x = (E, L (i, x))
+                           | otherwise = (L (i, x), E)
+        splitAux f (N _ l r) i = let
+                                    sL = size3 l
+                                    ((lN, lS), (rN, rS)) = splitAux f l i ||| splitAux f r (i+sL)
+                                 in
+                                    (append3 lN rN, append3 lS rS)
+
+fromListTree2 :: [String] -> Tree2 String
+fromListTree2 [] = E
+fromListTree2 [x] = L x
+fromListTree2 xs = let
+                     len = length xs
+                     m = len `div` 2
+                     (l, r) = splitAt m xs
+                   in
+                    N len (fromListTree2 l) (fromListTree2 r)
+
+{-
+Ejercicio final 4 - 2019
+-}
+data Cadena = E7 | L7 Char | N7 Cadena Cadena deriving Show
+
+palindromo :: Cadena -> Bool
+palindromo t = let 
+                (strOriginal, strReverse) = aplanar t ||| aplanar (reverseCadena t)
+               in
+                strOriginal == strReverse
+    where
+        reverseCadena :: Cadena -> Cadena
+        reverseCadena E7 = E7
+        reverseCadena (L7 x) = L7 x
+        reverseCadena (N7 l r) =  let
+                                (l', r') = reverseCadena l ||| reverseCadena r
+                            in
+                                N7 r' l'
+
+        aplanar :: Cadena -> String
+        aplanar E7 = ""
+        aplanar (L7 x) = [x]
+        aplanar (N7 l r) = let
+                            (l', r') = aplanar l ||| aplanar r
+                           in
+                            l' ++ r'
+
+crear :: Int -> (Int -> Char) -> Cadena
+crear i f = crear' 0 (i+1)
+    where
+        crear' inicio fin | inicio == fin = if inicio == 0 || inicio == i+1 then L7 '*' else L7 (f inicio)
+                          | otherwise = let
+                                            medio = (inicio+fin) `div` 2
+                                            (l, r) = crear' inicio medio ||| crear' (medio+1) fin
+                                          in
+                                            N7 l r
+
+{-
+Parcial 2 - 2023
+-}
+data Tree3 a = E8 | N8 Int (Tree3 a) a (Tree3 a)
+
+filterPrefix :: (a -> Bool) -> Tree3 a -> Tree3 a
+filterPrefix _ E8 = E8
+filterPrefix f (N8 _ l x r) = let
+                                (l', r') = filterPrefix f l ||| filterPrefix f r
+                             in
+                                if size4 l' == size4 l && f x then N8 (size4 l' + 1 + size4 r') l' x r' else l'
+    where
+        size4 :: Tree3 a -> Int
+        size4 E8 = 0
+        size4 (N8 s _ _ _) = s
+
+longestStreak :: Seq s => Int -> s Int -> Int
+longestStreak x s = let
+                        s_info = mapS (\i-> if i > x then 1 else 0) s
+                        s_inter = tabulateS (\i -> (nthS s_info i, i, nthS s_info i, i)) (lengthS s_info) `asTypeOf` mapS undefined s_info
+                        compare (0, -2, 0, -2) t = t
+                        compare t (0, -2, 0, -2) = t
+                        compare (l1, i1, r1, j1) (l2, i2, r2, j2) = if r1 == l2 && j1 == i2-1 then (l1, i1, r2, j2) else (l2, i2, r2, j2)
+                        (s_cont, r) = scanS compare (0, -2, 0, -2) s_inter
+                        s_app = appendS (dropS s_cont 1) (singletonS r)
+                        s_val = mapS (\(l, i, r, j) -> j-i+1) s_app
+                    in
+                        reduceS max 0 s_val
+
+{-
+Final 2 - 2019
+-}
+
+mapIndex :: (a -> Int -> b) -> Tree2 a -> Tree2 b
+mapIndex f t = mi f t 0
+    where
+        mi :: (a -> Int -> b) -> Tree2 a -> Int -> Tree2 b
+        mi _ E _ = E
+        mi f (L x) i = L (f x i)
+        mi f (N _ l r) i = let
+                            sL = size3 l
+                            (l', r') = mi f l i ||| mi f r (i+sL)
+                         in
+                            N (size3 l' + size3 r') l' r'
+
+fromSlow :: Int -> Int -> Int -> Tree2 Int
+fromSlow n m k = let
+                    arbol = crearArbol m
+                 in
+                    mapIndex (\_ i -> n + (i `div` k)) arbol
+    where
+        crearArbol :: Int -> Tree2 ()
+        crearArbol 0 = E
+        crearArbol 1 = L ()
+        crearArbol m =  let
+                            mitad = m `div` 2
+                            (l, r) = crearArbol mitad ||| crearArbol (m-mitad)
+                        in
+                            N m l r
+
+alarma :: Seq s => s (Int, String) -> Int -> Maybe String
+alarma s x | lengthS s == 0 = Nothing
+           | otherwise =  let
+                                s_idx = tabulateS id (lengthS s) `asTypeOf` mapS undefined s
+                                s_pesos = mapS fst s
+                                (s_sum, r) = scanS (+) 0 s_pesos
+                                s_app = appendS (dropS s_sum 1) (singletonS r)
+                                s_filter = filterS (\i-> nthS s_app i >= x) s_idx
+                            in
+                                if lengthS s_filter == 0 then Nothing else Just (snd (nthS s (nthS s_filter 0)))
+
+{-
+Final 2 - 2018
+-}
+data BST a = E9 | N9 (BST a) a (BST a) deriving Show
+
+delete :: Ord a => BST a -> a -> BST a
+delete E9 _ = E9
+delete (N9 l x r) y | x == y = case (l,r) of
+                                (E9, E9) -> E9
+                                (_, E9) -> l
+                                (E9, _) -> r
+                                (_, _) -> let y = minimum2 r in N9 l x (delete r y)
+                    | y < x = N9 (delete l y) x r
+                    | otherwise = N9 l x (delete r y)
+    where
+        minimum2 :: BST a -> a
+        minimum2 (N9 E9 x _) = x
+        minimum2 (N9 l _ _) = minimum2 l
+
+{-
+Parcial 2 - 2014
+-}
+concat2 :: Tree2 (Tree2 a) -> Tree2 a
+concat2 E = E
+concat2 (L x) = x
+concat2 (N _ l r) =  let
+                        (l', r') = concat2 l ||| concat2 r
+                    in
+                        N (size3 l' + size3 r') l' r'
+
+subsequence :: Tree2 a -> Int -> Int -> Tree2 a
+subsequence E _ _ = E
+subsequence (L x) i j | i == 0 && j >= 0 = L x
+                      | otherwise = E
+subsequence t@(N s l r) i j | i == 0 && j >= (s-1) = t
+                            | j < sL = subsequence l i j
+                            | i >= sL = subsequence r (i-sL) (j-sL)
+                            | otherwise = let
+                                            (l', r') = subsequence l i (sL-1) ||| subsequence r 0 (j-sL)
+                                          in
+                                            N (size3 l' + size3 r') l' r'
+    where sL = size3 l
+
+aa :: Seq s => s Char -> Int
+aa s =  let
+            s_info = tabulateS (\i -> if nthS s i == 'a' && nthS s (i+1) == 'a' then 1 else 0) (lengthS s-1) `asTypeOf` mapS undefined s
+        in
+            reduceS (+) 0 s_info
+
+{-
+Parcial 1 
+-}
+data Treap p k = E10 | N10 (Treap p k) p k (Treap p k)
+
+key :: Treap p k -> k
+key (N10 _ _ k _) = k
+
+priority :: Treap p k -> p
+priority (N10 _ p _ _) = p
+
+isTreap :: (Ord k, Ord p) => Treap p k -> Bool
+isTreap t = isBSTTreap t Nothing Nothing && isMaxHeap t
+    where
+        isBSTTreap :: Ord k => Treap p k -> Maybe k -> Maybe k -> Bool
+        isBSTTreap E10 _ _ = True
+        isBSTTreap (N10 l _ k r) minB maxB = isValid k minB maxB && isBSTTreap l minB (Just k) && isBSTTreap r (Just k) maxB
+
+        isValid val minB maxB = checkMin val minB && checkMax val maxB
+
+        checkMin val Nothing = True
+        checkMin val (Just x) = val >= x
+
+        checkMax val Nothing = True
+        checkMax val (Just x) = val <= x
+
+        isMaxHeap :: Ord p => Treap p k -> Bool
+        isMaxHeap E10 = True
+        isMaxHeap (N10 l p _ r) = checkHijo l p && checkHijo r p && isMaxHeap l && isMaxHeap r
+
+        checkHijo :: Ord p => Treap p k -> p -> Bool
+        checkHijo E10 _ = True
+        checkHijo (N10 _ p _ _) pPadre = pPadre >= p
+
+insertTreap :: (Ord k, Ord p) => k -> p -> Treap p k -> Treap p k
+insertTreap k p E10 = N10 E10 p k E10
+insertTreap k p (N10 l p' k' r) | k <= k' = if p > p' then rotateR (N10 (insertTreap k p l) p' k' r)
+                                                      else N10 (insertTreap k p l) p' k' r
+                                | otherwise = if p > p' then rotateL (N10 l p' k' (insertTreap k p r))
+                                                        else N10 l p' k' (insertTreap k p r)
+    where
+        rotateL (N10 l' p' k' (N10 l p k r)) = N10 (N10 l' p' k' l) p k r
+        rotateR (N10 (N10 l p k r) p' k' r') = N10 l p k (N10 r p' k' r')
+
+splitTreap :: (Ord k, Ord p, Num p) => k -> Treap p k -> (Treap p k, Treap p k)
+splitTreap _ E10 = (E10, E10)
+splitTreap k t = let
+                pMax = priority t
+                (N10 l _ _ r) = insertTreap k (pMax+1) t
+            in
+                (l,r)
